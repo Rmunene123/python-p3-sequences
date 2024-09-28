1c6eb688a9070fbae1adffcff050575adc0ba415
Rmunene123 <rosewanmun@gmail.com>
2024-09-29 00:33:59 +0300

    added sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+    
+    fib_sequence = [0, 1]
+    
+    for i in range(2, length):
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_fib)
+    
+    print(fib_sequence)
+
+# Example usage
+print_fibonacci(9)  # => [0, 1, 1, 2, 3, 5, 8, 13, 21]
